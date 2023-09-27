99aba8e144306f2520cfef46f369189ccce2340e
Astrokrisz <biro.krisztian.tamas@gmail.com>
2023-09-27 12:55:30 +0200

    DummySensor class created

---
+++ src/AutomatedCar/SystemComponents/Sensors/DummySensor.cs
@@ -0,0 +1,19 @@
+﻿namespace AutomatedCar.SystemComponents.Sensors
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+
+    public class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+
+        }
+        public override void Process()
+        {
+
+        }
+    }
+}
