5bc6e0eeebe7b380fbc8e68b368dc51d16a0792f
SeSa99 <sesa99@stud.uni-obuda.hu>
2023-10-23 20:59:21 +0200

    1st try

---
+++ src/AutomatedCar/SystemComponents/DummySensor.cs
@@ -0,0 +1,29 @@
+﻿namespace AutomatedCar.SystemComponents
+{
+    using AutomatedCar.Models;
+    using AutomatedCar.SystemComponents.Packets;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class DummySensor : SystemComponent
+    {
+        private DummyPacket dummyPacket;
+        public DummySensor(VirtualFunctionBus virtualFunctionBus) : base(virtualFunctionBus)
+        {
+            this.dummyPacket = new DummyPacket();
+            this.virtualFunctionBus.DummyPacket = this.dummyPacket;
+        }
+
+        public override void Process()
+        {
+            var circle = World.Instance.WorldObjects[0] as Circle;
+            var car = World.Instance.ControlledCar;
+
+            this.dummyPacket.DistanceX = Math.Abs(car.X - circle.X);
+            this.dummyPacket.DistanceY = Math.Abs(car.Y - circle.Y);
+        }
+    }
+}
